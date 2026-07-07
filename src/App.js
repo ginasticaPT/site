7466665aca2c46a1a1921ce7aec5e717c974e0b9
ginasticaPT <300928663+ginasticaPT@users.noreply.github.com>
2026-07-07 15:53:51 +0100

    Fix closing tag for h2 element in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Bem-vindos</h1>
-        <h2>Uma história contada através dos factos</h1>
+        <h2>Uma história contada através dos factos</h2>
         <p>Uma viagem através de decisões, acontecimentos e momentos que moldaram o processo eleitoral e Assembleia Geral da Federação de Ginástica de Portugal desde 2024. Cada evento é acompanhado pelas evidências e documentos disponíveis, permitindo-lhe construir a sua própria interpretação dos factos.</p>
       </header>
     </div>
