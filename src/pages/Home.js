59329a6ec144f36ac1181b1cd9599e75616720d9
ginasticaPT <300928663+ginasticaPT@users.noreply.github.com>
2026-07-09 12:23:32 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,6 @@ function Home() {
       <header className="App-header">
         <h1>O PROCESSO DA FEDERAÇÃO DE GINÁSTICA DE PORTUGAL</h1>
         <h2>Uma cronologia contada através dos factos</h2>
-        <p>Uma viagem através de decisões, acontecimentos e momentos que moldaram o processo eleitoral e a Assembleia Geral da Federação de Ginástica de Portugal desde 2024.</p>
-        <p>Cada evento é acompanhado pelas evidências e documentos disponíveis, permitindo-lhe construir a sua própria interpretação dos factos.</p>
         <Link to="/timeline">
           <button className="start-button">Iniciar</button>
         </Link>
