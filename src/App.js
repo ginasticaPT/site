446f4670cf0f196e89d54b1a93084936699da3f9
ginasticaPT <300928663+ginasticaPT@users.noreply.github.com>
2026-07-09 12:07:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bem-vindos</h1>
+        <h1>FEDERAÇÃO DE GINÁSTICA DE PORTUGAL - PROCESSO TAD, IPDJ e AG</h1>
         <h2>Uma história contada através dos factos</h2>
-        <p>Uma viagem através de decisões, acontecimentos e momentos que moldaram o processo eleitoral e Assembleia Geral da Federação de Ginástica de Portugal desde 2024. Cada evento é acompanhado pelas evidências e documentos disponíveis, permitindo-lhe construir a sua própria interpretação dos factos.</p>
+        <p>Uma viagem através de decisões, acontecimentos e momentos que moldaram o processo eleitoral e os acontecimentos da Assembleia Geral da Federação de Ginástica de Portugal desde 2024. Cada evento é acompanhado pelas evidências e documentos disponíveis, permitindo-lhe construir a sua própria interpretação dos factos.</p>
       </header>
     </div>
   );
