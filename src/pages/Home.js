fcab8d9a378a84666031120ce76814846d3f4980
ginasticaPT <300928663+ginasticaPT@users.noreply.github.com>
2026-07-10 10:37:31 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>FGP 2024–2028: O Processo que Conduziu ao processo no TAD e no IPDJ</h1>
+        <h1>FGP 2025–2028: O Processo que Conduziu ao processo no TAD e no IPDJ</h1>
         <h2>Uma cronologia contada através dos factos</h2>
         <Link to="/timeline">
           <button className="start-button">Iniciar</button>
