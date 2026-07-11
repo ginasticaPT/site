90e50a0c74b86895aefbda95bfe6cacbf95ec4a4
ginasticaPT <300928663+ginasticaPT@users.noreply.github.com>
2026-07-11 21:08:02 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+﻿import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Timeline from './pages/Timeline';
+import Overview from './pages/Overview';
+import Timeline2024 from './pages/Timeline2024';
+import Timeline2025 from './pages/Timeline2025';
+import Timeline2026 from './pages/Timeline2026';
 
 function App() {
   return (
     <Router basename="/site">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/timeline/2024" element={<Timeline2024 />} />
+        <Route path="/timeline/2025" element={<Timeline2025 />} />
+        <Route path="/timeline/2026" element={<Timeline2026 />} />
       </Routes>
     </Router>
   );
